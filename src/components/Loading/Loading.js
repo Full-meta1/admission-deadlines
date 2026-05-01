c6e0f11b5d79a96561c4a33d4f1052dc0037681f
Joseph <benoitballs24@yahoo.com>
2026-05-01 16:47:11 -0400

    changed no result message

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -7,7 +7,7 @@ let Loading = (data) => {
     return (
         <div>
             { (data.data.timeout===true) 
-            ? <div style={{textAlign:'center', display: 'flex', alignItems: 'center', minHeight: '185px', justifyContent: 'center'}}><span><p>Sorry, No course information is available for {data.data.courseCode} at this time. Please check back at a later date.</p><br /></span></div>
+            ? <div style={{textAlign:'center', display: 'flex', alignItems: 'center', minHeight: '185px', justifyContent: 'center'}}><span><p>Sorry, No admission deadlines are currently available. Please check back at a later date.</p><br /></span></div>
             : <div style={{textAlign:'center', display: 'flex', alignItems: 'center', minHeight: '185px', justifyContent: 'center', backgroundColor: '#D3D3D3'}}><span className='loading'></span></div> 
             }
         </div>
